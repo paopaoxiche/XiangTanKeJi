c9326250725837b902fcff879c4e242d56c53961
ranger <liqk@fsmeeting.com>
2018-07-09 18:20:56 +0800

    create android project

---
+++ android/app/src/main/java/com/xtkj/paopaoxiche/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.xtkj.paopaoxiche.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xtkj.paopaoxiche.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
