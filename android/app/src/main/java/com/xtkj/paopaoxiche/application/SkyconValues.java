875d1cdaaf719ea0552166ec6ec485fe9d1e8ce0
Bingo <haohaoxuexi>
2018-08-11 14:27:50 +0800

    提交代码 2018 8 11 14:27

--- android/app/src/main/java/com/xtkj/paopaoxiche/application/SkyconValues.java
+++ android/app/src/main/java/com/xtkj/paopaoxiche/application/SkyconValues.java
@@ -11,6 +11,9 @@ public class SkyconValues {
     public static final Map<String,Integer> homeIconMap = new HashMap<String,Integer>() ;
     public static final Map<String,Integer> weatherBgMap = new HashMap<String,Integer>() ;
 
+    public static final Map<String,Integer> forecastBgMap = new HashMap<String,Integer>() ;
+    public static final Map<String,Integer> forecastIconMap = new HashMap<String,Integer>() ;
+
     static {
 
         cnNameMap.put("CLEAR_DAY","晴天") ;
@@ -44,5 +47,26 @@ public class SkyconValues {
         weatherBgMap.put("SNOW", R.drawable.bg_weather_snow);
         weatherBgMap.put("WIND", R.drawable.bg_weather_windy);
         weatherBgMap.put("HAZE",R.drawable.bg_weather_haze);
+
+
+        forecastBgMap.put("CLEAR_DAY", R.drawable.bg_forecast_good_day);
+        forecastBgMap.put("CLEAR_NIGHT", R.drawable.bg_forecast_good_night);
+        forecastBgMap.put("PARTLY_CLOUDY_DAY", R.drawable.bg_forecast_cloudy);
+        forecastBgMap.put("PARTLY_CLOUDY_NIGHT", R.drawable.bg_forecast_cloudy);
+        forecastBgMap.put("CLOUDY", R.drawable.bg_forecast_cloudy);
+        forecastBgMap.put("RAIN", R.drawable.bg_forecast_rain);
+        forecastBgMap.put("SNOW", R.drawable.bg_forecast_snow);
+        forecastBgMap.put("WIND", R.drawable.bg_forecast_wind);
+        forecastBgMap.put("HAZE", null);
+
+        forecastIconMap.put("CLEAR_DAY", R.drawable.forecast_good_day);
+        forecastIconMap.put("CLEAR_NIGHT", R.drawable.forecast_good_night);
+        forecastIconMap.put("PARTLY_CLOUDY_DAY", R.drawable.forecast_cloudy_day);
+        forecastIconMap.put("PARTLY_CLOUDY_NIGHT", R.drawable.forecast_cloudy_night);
+        forecastIconMap.put("CLOUDY", R.drawable.forecast_cloudy);
+        forecastIconMap.put("RAIN", R.drawable.forecast_rain);
+        forecastIconMap.put("SNOW", R.drawable.forecast_snow);
+        forecastIconMap.put("WIND", R.drawable.forecast_wind);
+        forecastIconMap.put("HAZE",R.drawable.forecast_haze);
     }
 }
--- android/app/src/main/java/com/xtkj/paopaoxiche/view/WeatherForecast/WeatherForecastActivity.java
+++ android/app/src/main/java/com/xtkj/paopaoxiche/view/WeatherForecast/WeatherForecastActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.xtkj.paopaoxiche.R;
@@ -14,8 +15,10 @@ import com.xtkj.paopaoxiche.bean.WeatherForecastBean;
 import com.xtkj.paopaoxiche.bean.WeatherRealTimeBean;
 import com.xtkj.paopaoxiche.contract.IWeatherForecastContract;
 import com.xtkj.paopaoxiche.presenter.WeatherForecastPresenterImpl;
+import com.xtkj.paopaoxiche.widget.MarqueeTextView;
 
 import java.text.DecimalFormat;
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,12 +26,14 @@ import butterknife.ButterKnife;
 public class WeatherForecastActivity extends BaseGaodeActivity implements IWeatherForecastContract.IWeatherForecastView {
 
     IWeatherForecastContract.IWeatherForecastPresenter weatherForecastPresenterImpl = null;
+    private static final String[] WEEK_NAME = {"星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"};
+
     @BindView(R.id.back_button)
     ImageView backButton;
     @BindView(R.id.location_text)
     TextView locationText;
     @BindView(R.id.bg_weather)
-    LinearLayout bgWeather;
+    ScrollView bgWeather;
     @BindView(R.id.temperature)
     TextView temperature;
     @BindView(R.id.temperature_high)
@@ -39,6 +44,62 @@ public class WeatherForecastActivity extends BaseGaodeActivity implements IWeath
     TextView wind;
     @BindView(R.id.humidity)
     TextView humidity;
+    @BindView(R.id.skycon)
+    TextView skycon;
+    @BindView(R.id.forecast)
+    MarqueeTextView forecast;
+    @BindView(R.id.date1)
+    TextView date1;
+    @BindView(R.id.week1)
+    TextView week1;
+    @BindView(R.id.sky_img)
+    ImageView skyImg;
+    @BindView(R.id.high1)
+    TextView high1;
+    @BindView(R.id.low1)
+    TextView low1;
+    @BindView(R.id.date2)
+    TextView date2;
+    @BindView(R.id.week2)
+    TextView week2;
+    @BindView(R.id.sky_img2)
+    ImageView skyImg2;
+    @BindView(R.id.high2)
+    TextView high2;
+    @BindView(R.id.low2)
+    TextView low2;
+    @BindView(R.id.date3)
+    TextView date3;
+    @BindView(R.id.week3)
+    TextView week3;
+    @BindView(R.id.sky_img3)
+    ImageView skyImg3;
+    @BindView(R.id.high3)
+    TextView high3;
+    @BindView(R.id.low3)
+    TextView low3;
+    @BindView(R.id.date4)
+    TextView date4;
+    @BindView(R.id.week4)
+    TextView week4;
+    @BindView(R.id.sky_img4)
+    ImageView skyImg4;
+    @BindView(R.id.high4)
+    TextView high4;
+    @BindView(R.id.low4)
+    TextView low4;
+    @BindView(R.id.date5)
+    TextView date5;
+    @BindView(R.id.week5)
+    TextView week5;
+    @BindView(R.id.sky_img5)
+    ImageView skyImg5;
+    @BindView(R.id.high5)
+    TextView high5;
+    @BindView(R.id.low5)
+    TextView low5;
+    @BindView(R.id.forecast_bg)
+    LinearLayout forecastBg;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -87,6 +148,8 @@ public class WeatherForecastActivity extends BaseGaodeActivity implements IWeath
         wind.setText(String.format("风速%s", myformat1.format(weatherRealTimeBean.getResult().getWind().getSpeed())));
         humidity.setText(String.format("湿度%s%%", myformat1.format(weatherRealTimeBean.getResult().getHumidity() * 100)));
         bgWeather.setBackgroundResource(SkyconValues.weatherBgMap.get(weatherRealTimeBean.getResult().getSkycon()));
+        skycon.setText(SkyconValues.cnNameMap.get(weatherRealTimeBean.getResult().getSkycon()));
+        forecastBg.setBackgroundResource(SkyconValues.forecastBgMap.get(weatherRealTimeBean.getResult().getSkycon()));
     }
 
     @Override
@@ -95,6 +158,39 @@ public class WeatherForecastActivity extends BaseGaodeActivity implements IWeath
         DecimalFormat myformat0 = new DecimalFormat("0");
         temperatureHigh.setText(String.format("%s°", myformat0.format(weatherForecastBean.getResult().getDaily().getTemperature().get(0).getMax())));
         temperatureLow.setText(String.format("%s°", myformat0.format(weatherForecastBean.getResult().getDaily().getTemperature().get(0).getMin())));
+        forecast.setText(weatherForecastBean.getResult().getForecast_keypoint());
+
+        date1.setText(weatherForecastBean.getResult().getDaily().getTemperature().get(0).getDate());
+        date2.setText(weatherForecastBean.getResult().getDaily().getTemperature().get(1).getDate());
+        date3.setText(weatherForecastBean.getResult().getDaily().getTemperature().get(2).getDate());
+        date4.setText(weatherForecastBean.getResult().getDaily().getTemperature().get(3).getDate());
+        date5.setText(weatherForecastBean.getResult().getDaily().getTemperature().get(4).getDate());
+
+        Calendar cal = Calendar.getInstance();
+        int i = cal.get(Calendar.DAY_OF_WEEK);
+        week1.setText("今天");
+        week2.setText(WEEK_NAME[(i) % 7]);
+        week3.setText(WEEK_NAME[(i + 1) % 7]);
+        week4.setText(WEEK_NAME[(i + 2) % 7]);
+        week5.setText(WEEK_NAME[(i + 3) % 7]);
+
+        skyImg.setImageResource(SkyconValues.forecastIconMap.get(weatherForecastBean.getResult().getDaily().getSkycon().get(0).getValue()));
+        skyImg2.setImageResource(SkyconValues.forecastIconMap.get(weatherForecastBean.getResult().getDaily().getSkycon().get(1).getValue()));
+        skyImg3.setImageResource(SkyconValues.forecastIconMap.get(weatherForecastBean.getResult().getDaily().getSkycon().get(2).getValue()));
+        skyImg4.setImageResource(SkyconValues.forecastIconMap.get(weatherForecastBean.getResult().getDaily().getSkycon().get(3).getValue()));
+        skyImg5.setImageResource(SkyconValues.forecastIconMap.get(weatherForecastBean.getResult().getDaily().getSkycon().get(4).getValue()));
+
+        high1.setText(myformat0.format(weatherForecastBean.getResult().getDaily().getTemperature().get(0).getMax()));
+        high2.setText(myformat0.format(weatherForecastBean.getResult().getDaily().getTemperature().get(1).getMax()));
+        high3.setText(myformat0.format(weatherForecastBean.getResult().getDaily().getTemperature().get(2).getMax()));
+        high4.setText(myformat0.format(weatherForecastBean.getResult().getDaily().getTemperature().get(3).getMax()));
+        high5.setText(myformat0.format(weatherForecastBean.getResult().getDaily().getTemperature().get(4).getMax()));
+
+        low1.setText(myformat0.format(weatherForecastBean.getResult().getDaily().getTemperature().get(0).getMin()));
+        low2.setText(myformat0.format(weatherForecastBean.getResult().getDaily().getTemperature().get(1).getMin()));
+        low3.setText(myformat0.format(weatherForecastBean.getResult().getDaily().getTemperature().get(2).getMin()));
+        low4.setText(myformat0.format(weatherForecastBean.getResult().getDaily().getTemperature().get(3).getMin()));
+        low5.setText(myformat0.format(weatherForecastBean.getResult().getDaily().getTemperature().get(4).getMin()));
 
     }
 
